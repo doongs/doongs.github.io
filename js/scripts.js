2ec8159527736d830f89a3d2bfc08ac44f85a80a
Gabriel Doon <doongs@dukes.jmu.edu>
2021-03-29 14:56:20 -0400

    more bg images

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-let images = ["cspresent.jpg", "cspresent2.jpeg", "cspresent3.jpeg"];
+let images = ["cspresent.jpg", "cspresent2.jpeg", "cspresent3.jpeg", "mrdsax.jpg", "jazz.jpg"];
 
 (() => {
-  var item = images[Math.floor(Math.random() * images.length)];
   document.getElementById("main-bg").style =
-    `background-image: url('https://doongs.github.io/img/${item}'); height: 100vh`;
+    `background-image: url('https://doongs.github.io/img/${images[Math.floor(Math.random() * images.length)]}'); height: 100vh`;
 })();
